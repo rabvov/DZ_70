da9dc85b4629a5b920a616bc81c739f5ea89e30e
rak15 <rak1529916@mail.ru>
2023-04-21 21:26:20 +0300

    1.1

---
+++ src/DZ_70.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DZ_70 {
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        scanner.close();
+
+        try {
+            if (a == 0)
+                throw new NullPointerException();
+            if (b==0)
+                throw new ArithmeticException();
+        }
+        catch (ArithmeticException arithmeticException) {
+            System.out.println("Деление на 0");
+        }
+        catch (NullPointerException e) {
+            System.out.println("0 всегда будет 0");
+        }
+
+        try {
+            if (a-b<0)
+                throw new ArithmeticException();
+            if (a==0&&b<0)
+                throw new Exception();
+        } catch (Exception arithmeticException){
+            System.out.println("Отрицательное значение");
+        }
+
+
+    }
+}
